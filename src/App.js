5831dfad6b519dacb84b440472ebb81d26d791d0
hirokianzai <hirokianzai511@gmail.com>
2019-01-27 00:41:50 +0900

    Learned jsx syntax

--- src/App.js
+++ src/App.js
@@ -4,23 +4,11 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Hello World!
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+    return(
+      <React.Fragment>
+        <label htmlFor="bar">bar</label>
+        <input type="text" onClick={() => {console.log("I am clicked");}}/>
+      </React.Fragment>
     );
   }
 }
