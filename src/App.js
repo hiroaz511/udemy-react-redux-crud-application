0e46d4b04139b084f9f179e86e89244657c7bfb0
hirokianzai <hirokianzai511@gmail.com>
2019-01-27 00:59:07 +0900

    learned components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return(
-      <React.Fragment>
-        <label htmlFor="bar">bar</label>
-        <input type="text" onClick={() => {console.log("I am clicked");}}/>
-      </React.Fragment>
-    );
-  }
+// class App extends Component {
+//   render() {
+//     return(
+//       <React.Fragment>
+//         <label htmlFor="bar">bar</label>
+//         <input type="text" onClick={() => {console.log("I am clicked");}}/>
+//       </React.Fragment>
+//     );
+//   }
+// }
+
+const App = () => {
+  return(
+  <div>
+    <Cat />
+    <Dog />
+  </div>
+  );
+}
+
+const Cat = () => {
+  return(<div>Meow!!</div>);
+}
+const Dog = () => {
+  return(<div>Bow!!</div>);
 }
 
 export default App;
